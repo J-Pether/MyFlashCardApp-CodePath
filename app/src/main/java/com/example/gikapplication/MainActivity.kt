1d5ba367f1ba8a21c2ebb10f1c49a339672cb84f
wilderedarius-ui <wilderedarius@gmail.com>
2025-10-29 18:54:14 -0400

    first

---
+++ app/src/main/java/com/example/gikapplication/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.gikapplication
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.flashcard_question).setOnClickListener {
+            findViewById<View>(R.id.flashcard_question).visibility=View.INVISIBLE
+            findViewById<View>(R.id.flashcard_response).visibility=View.VISIBLE
+
+        }
+        findViewById<View>(R.id.flashcard_response).setOnClickListener {
+            findViewById<View>(R.id.flashcard_question).visibility=View.VISIBLE
+            findViewById<View>(R.id.flashcard_response).visibility=View.INVISIBLE
+
+        }
+
+
+//
+
+//
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
